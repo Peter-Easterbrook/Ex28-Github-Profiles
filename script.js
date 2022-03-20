63dd24a1cf401b1bd9beb100aaa2d2f5520229d4
Peter-Easterbrook <peter.easterbrook@gmail.com>
2022-03-20 15:47:22 +0100

    UI tweaks

--- script.js
+++ script.js
@@ -28,18 +28,20 @@ async function getRepos(username) {
 }
 
 function createUserCard(user) {
+  const userID = user.name || user.login;
+  const userBio = user.bio ? `<p>${user.bio}</p>` : '';
   const cardHTML = `
     <div class="card">
     <div>
       <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
     </div>
     <div class="user-info">
-      <h2>${user.name}</h2>
-      <p>${user.bio}</p>
+    <h2>${userID}</h2>
+    ${userBio}
       <ul>
-        <li>${user.followers} <strong>Followers</strong></li>
-        <li>${user.following} <strong>Following</strong></li>
-        <li>${user.public_repos} <strong>Repos</strong></li>
+        <li>${user.followers}   Followers  </li>
+        <li>${user.following}   Following  </li>
+        <li>${user.public_repos}   Repos  </li>
       </ul>
 
       <div id="repos"></div>
